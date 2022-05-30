011ab82b57a3ba3f491177303103d56fa6b3ed13
Core2-0 <Core2-00@mail.ru>
2022-05-30 14:04:45 +0500

    2.10. Util functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
+'use strict';
 
+const getRandomNumber = (min, max, accuracy) => {
+  min = parseFloat(min);
+  max = parseFloat(max);
+  accuracy = parseInt(accuracy);
+
+  if (min < 0 || max < 0 || accuracy < 0) {
+    throw new TypeError('Допустимы только положительные числа!');
+  }
+
+  if (isNaN(min) || isNaN(max) || isNaN (accuracy)) {
+    throw new TypeError('Допускаются только числовые значения!')
+  }
+
+  if (parseInt(min) === parseInt(max) && accuracy === 0) {
+    if(parseInt(min) !== min && parseInt(max) !== max) {
+      throw new TypeError('Выполнение операции невозможно!');
+    }
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  const power = Math.pow(10 , accuracy);
+  const start = Math.ceil(min * power) / power;
+  const end = Math.floor(max * power) / power;
+
+  return (Math.random() * (end - start) + start).toFixed(accuracy);
+};
+
+const getRandomInt = (min, max) => {
+  return parseInt(getRandomNumber(min, max, 0));
+};
+
+const getRandomFloat = (min, max, accuracy) => {
+  return parseFloat(getRandomNumber(min, max , accuracy));
+};
+
+getRandomInt(4.4, 4);
+getRandomFloat(3.2, 3.3, 2);
